ff0b9e629ae0d3ec11686730afc2e3440767a3e6
24020292 <24020292@vnu.edu.vn>
2025-09-23 11:01:00 +0700

    B

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đỗ Minh Sơn\t24020292\t2526I_INT2204_4\tmson24020292\tsonlinh0520@gmail.com");
+        for (int i = 9; i > 1; i--) {
+            System.out.println( i + " bottles of beer on the wall, " + i  + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
